3f78f92aed59163e25dd478af188bcb955fde996
Esteban Muñoz <estebanmunozrojas@gmail.com>
2022-10-12 22:08:20 -0300

    testttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter ,Route,Routes} from 'react-router-dom'
+import { BrowserRouter ,Route,Routes, useNavigate} from 'react-router-dom'
 import './App.css'
 import banner from '../assets/banner.png'
 import ComponenteIngreso from './componentes/ComponenteIngreso'
@@ -8,21 +8,24 @@ import ComponenteProducto from './componentes/ComponenteProducto'
 import ComponenteGestion from './componentes/ComponenteGestion'
 import ComponenteVerCatalogo from './componentes/ComponenteVerCatalogo'
 import ComponenteIniciarSesion from './componentes/ComponenteIniciarSesion'
-import ComponenteHome from './componentes/ComponenteHome'
+
 
 
 
 function App() {
+
+  
   
 
   return (
     <BrowserRouter> 
+    
     <div className="App">
       <img src={banner} alt="" width="85%" height="85%" />
       <nav className="navbar   navbar-expand-lg  bg-light">
   <div className="container-fluid">
-    <a href="catalogo" className='catologo '> Ver catálogo</a>
-    <a href="iniciar-sesion" className=' sesion'>Iniciar sesión</a>
+    <a href="/catalogo" className='catologo '> Ver catálogo</a>
+    <a href="/iniciar-sesion" className=' sesion'>Iniciar sesión</a>
     
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
@@ -31,12 +34,12 @@ function App() {
         </li>
    
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Gestión de Usuarios
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="usuario">Mantenedor usuarios</a></li>
-            <li><a className="dropdown-item" href="ingreso">Ingreso usuario</a></li>
+            <li><a className="dropdown-item" href="/usuario" >Mantenedor usuarios</a></li>
+            <li><a className="dropdown-item" href="/ingreso">Ingreso usuario</a></li>
             
           </ul>
         </li>
@@ -49,8 +52,8 @@ function App() {
             Gestión de productos
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="ingreso-productos">Ingreso de Productos</a></li>
-            <li><a className="dropdown-item" href="gestion-productos">Gestión de productos</a></li>
+            <li><a className="dropdown-item" href="/ingreso-productos">Ingreso de Productos</a></li>
+            <li><a className="dropdown-item" href="/gestion-productos">Gestión de productos</a></li>
             
           </ul>
         </li>
@@ -58,7 +61,7 @@ function App() {
       <ul className="navbar-nav">
         
       <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Gestión de Ventas</a>
+          <a className="nav-link active" aria-current="page" href="/#">Gestión de Ventas</a>
         </li>
       </ul>
     </div>
